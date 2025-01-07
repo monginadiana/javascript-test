89b7eb5f63a53c929358eda243a72133eb5e9ab4
Monginadee <dianahmongina2@gmail.com>
2025-01-07 10:59:12 +0300

    listed items correctly test passed

--- src/List.test.js
+++ src/List.test.js
@@ -2,12 +2,11 @@ import { render, screen } from '@testing-library/react';
 import List from './List';
 
 test('Component List', () => {
-    it("render items", () => {
         const people = ['john', 'bob', 'jane'];
         render(<List items={people} />)
         const itemlist = screen.getAllByRole('listitem')
         expect(itemlist).toHaveLength(people.length)
-    })
+
     // const linkElement = screen.getByText(/learn react/i);
     // expect(linkElement).toBeInTheDocument();
 });
